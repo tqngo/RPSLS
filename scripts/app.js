6c259ba096fba4679be0510ec2462a074e17a033
Tommy Ngo <tommyquocngo@gmail.com>
2024-01-19 01:01:29 -0800

    js

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+let pBtn = document.getElementById("pBtn");
+let comBtn = document.getElementById("comBtn");
+let howBtn = document.getElementById("howBtn");
+
+document.getElementById("pBtn").addEventListener("click", function() {
+
+});
+
